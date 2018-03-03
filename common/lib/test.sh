476fc7a1db450ac7d6bde29c1d66c4bd6593731e
xnslong <szlon@qq.com>
2018-03-03 21:59:19 +0800

    support rerun

--- common/lib/test.sh
+++ common/lib/test.sh
@@ -2,9 +2,10 @@
 
 function run_test () {
     echo -e "-------------------------" >> "$LOG_FILE"
-    CASE_NAME="`yellow $1` (in `yellow $SUITE`)"
+    CASE_NAME="`yellow $1` in `yellow $SUITE`"
     info "running testcase $CASE_NAME" >> "$LOG_FILE"
     {
+        set -e
         "$1" &>> "$LOG_FILE"
     } && {
         msg="$CASE_NAME   `green "[SUCCESS]"`" 
@@ -20,12 +21,16 @@ function run_test () {
 
 function run_tests () {
     source "$1"
-    export SUITE="$(echo "$1" | sed -E 's#(.*/)*([^/]+)$#\2#g')"
-    export LOG_FILE="$LOG_DIR/$SUITE.log"
+    cases="$2"
+    export SUITE="$(echo "$1")"
+    export LOG_FILE="$LOG_DIR/$(echo "$SUITE.log" | sed -E 's#(.*/)*([^/]+)$#\2#g')"
     echo  -e "test suite \"$SUITE\" (log details see $LOG_FILE)"
     declare -F | awk '{print $3}' | while read name; do
         if echo "$name" | grep -E '^test_.*' &> /dev/null; then
             export CASE="$name"
+            if [ -n "$cases" ] && ! echo "$cases" | grep "$CASE" &> /dev/null; then
+                continue
+            fi
             run_test "$CASE" | awk '{print "\t" $0}' 
             unset CASE
         fi
--- run.sh
+++ run.sh
@@ -5,18 +5,55 @@ LOG_DIR="logs/$TEST_TIME"
 [ -d "$LOG_DIR" ] || mkdir -p "$LOG_DIR"
 
 export REPORT="$LOG_DIR/report.log"
-for l in `tree -f common | awk '{print $NF}'`; do
+for l in `find common -name '*.sh'`; do
     if echo "$l" | grep -E '.*\.sh$' &> /dev/null; then
         source "$l"
     fi
 done
 
-for f in cases/*; do
-    if echo "$f" | grep -E '.*\.case' &> /dev/null; then 
+function run_all {
+    for f in `find cases -name '*.case'`; do
         run_tests "$f" | tee -a "$REPORT"
-    fi
-done
+    done
 
-echo -e "see report $REPORT"
+    echo -e "see report $REPORT"
+}
+
+function run_failed {
+    report="$1"
+    failures="$(cat "$1" | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g"| awk '$4 ~ /.*FAIL.*/ {print $1, $3}')"
+    failure_file="$(echo "$failures" | awk '{print $2}' | sort | uniq)"
+
+    for f in `find cases -name '*.case'`; do
+        if echo "$failure_file" | grep "$f"; then 
+            run_tests "$f" "$(echo "$failures" | grep "$f" | awk '{print $1}')" | tee -a "$REPORT"
+        fi
+    done
+
+    echo -e "see report $REPORT"
+}
+
+function print_usage {
+cat << EOFSDFKJ
+$0                           run all test cases
+$0  -p <report file>         run all failed test cases in the report file
+$0  -h                       show this help and exit
+EOFSDFKJ
+}
+
+if [ $# -gt 0 ]; then
+    case "$1" in
+        -h)
+            print_usage
+            ;;
+        -p)
+            run_failed "$2"
+            ;;
+        *)
+            run_all
+    esac
+else
+    run_all
+fi
 
 unset TEST_TIME
